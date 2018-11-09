e2648b0ca044cfd54beb1d30aa737e5d52d02a0a
Théo de Paula <36240759+theoadepaula@users.noreply.github.com>
2018-11-09 18:08:46 -0200

    Inserção dos bancos de dados referente às rodadas 2 a 15.

--- RCODE/webscrape_campeonato_brasileiro.R
+++ RCODE/webscrape_campeonato_brasileiro.R
@@ -11,11 +11,11 @@ remDr$navigate("https://www.cartolafcbrasil.com.br/scouts/cartola-fc-2018/rodada
 opc_status=remDr$findElements(using="css","#ctl00_cphMainContent_drpStatus option")
 opc_status[[1]]$clickElement()
 Sys.sleep(1)
-#Achar o filtro de Itens por página e alterar para '100'
+#Achar o filtro de Itens por pÃ¡gina e alterar para '100'
 opc_ipg=remDr$findElements(using="css","#ctl00_cphMainContent_drpPageSize option")
 opc_ipg[[3]]$clickElement()
 Sys.sleep(1)
-#Achar o botão filtrar e apertar
+#Achar o bot?o filtrar e apertar
 bot_filtro1=remDr$findElements(using="css","#ctl00_cphMainContent_btnFiltrar")
 bot_filtro1[[1]]$clickElement()
 Sys.sleep(1)
@@ -52,7 +52,7 @@ cb$Clube=cbclube
 cb1 = cb
 cb1[is.na(cb1)]=0
 
-cb2= cb1 %>% separate(Nome, into =  c("Nome","Posição"),sep=" \\(") %>% mutate(Posição=str_replace(Posição,"\\)",""))
+cb2= cb1 %>% separate(Nome, into =  c("Nome","PosiÃ§Ã£o"),sep=" \\(") %>% mutate(PosiÃ§Ã£o=str_replace(PosiÃ§Ã£o,"\\)",""))
 
 #Escrevendo os bancos de dados em CSV
 write.csv(cb,"camp_bras_rodada_1_bruto.csv",fileEncoding = 'ISO-8859-1')
@@ -69,33 +69,33 @@ write.csv(cb2,"camp_bras_rodada_1_final.csv",fileEncoding = 'ISO-8859-1')
 # 
 # G - Gol (+8,0)
 # 
-# A - Assistência (+5,0)
+# A - AssistÃªncia (+5,0)
 # 
 # SG - Jogos sem sofrer gols (+5,0)
 # 
 # FS - Falta sofrida ( +0,5)
 # 
-# FF - Finalização para fora (+0,8)
+# FF - FinalizaÃ§Ã£o para fora (+0,8)
 # 
-# FD - Finalização defendida (+1,2)
+# FD - FinalizaÃ§Ã£o defendida (+1,2)
 # 
-# FT - Finalização na trave (+3,0)
+# FT - FinalizaÃ§Ã£o na trave (+3,0)
 # 
-# DD - Defesa difícil (+3,0)
+# DD - Defesa difÃ­cil (+3,0)
 # 
-# DP - Defesa de pênalti (+7,0)
+# DP - Defesa de pÃªnalti (+7,0)
 # 
 # SCOUTS NEGATIVOS
 # 
 # GC - Gol contra (-5,0)
 # 
-# CV - Cartão vermelho (-5,0)
+# CV - CartÃ£o vermelho (-5,0)
 # 
-# CA - Cartão amarelo (-2,0)
+# CA - CartÃ£o amarelo (-2,0)
 # 
 # GS - Gol sofrido (-2,0)
 # 
-# PP - Pênalti perdido (-4,0)
+# PP - PÃªnalti perdido (-4,0)
 # 
 # FC - Falta cometida (-0,5)
 # 
@@ -103,9 +103,9 @@ write.csv(cb2,"camp_bras_rodada_1_final.csv",fileEncoding = 'ISO-8859-1')
 # 
 # PE - Passe errado (-0,3)
 
-#análise dos dados da primeira rodada
+#anÃ¡lise dos dados da primeira rodada
 
-cb2 %>% count(Posição)
+cb2 %>% count(PosiÃ§Ã£o)
 
 cb2 %>% group_by(Nome,Clube) %>% summarise(total_rb=sum(RB)) %>% arrange(desc(total_rb))
 
