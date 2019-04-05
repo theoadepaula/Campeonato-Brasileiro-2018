6b3647d72e5db084e21b49ed460d8528333117cc
Théo de Paula <36240759+theoadepaula@users.noreply.github.com>
2019-04-05 17:55:43 -0300

    05/04/2019

--- RCODE/captura_manual.R
+++ RCODE/captura_manual.R
@@ -1,4 +1,5 @@
-remDr$navigate("https://www.cartolafcbrasil.com.br/scouts/cartola-fc-2018/rodada-20")
+nrod=22
+remDr$navigate(paste0("https://www.cartolafcbrasil.com.br/scouts/cartola-fc-2018/rodada-",nrod))
 
 #Achar o filtro de Status e alterar para 'Todos'
 opc_status=remDr$findElements(using="css","#ctl00_cphMainContent_drpStatus option")
@@ -25,26 +26,31 @@ for(i in 1:8){
   Sys.sleep(2)
 }
 
-cb[[8]]
-clube[[8]]
-
-for(i in 7:8){
+for(i in 2:8){
   remDr$executeScript(paste0("javascript:__doPostBack('ctl00$cphMainContent$gvList','Page$",i,"')"))
   Sys.sleep(2)
   page <- read_html(remDr$getPageSource()[[1]])
+  cb[[i]]=page %>% html_node(".table") %>% html_table(fill=T) %>% .[1:100,1:25]
   clube[[i]]=page %>% html_node(".table") %>% html_nodes("img") %>% html_attr("alt")
   Sys.sleep(2)
 }
 
+
+cb[[2]] %>% slice(1:10) %>% select(Nome)
+clube[[8]]
+
+
 remDr$executeScript(paste0("javascript:__doPostBack('ctl00$cphMainContent$gvList','Page$",9,"')"))
 page <- read_html(remDr$getPageSource()[[1]])
-cb[[9]]=page %>% html_node(".table") %>% html_table(fill=T) %>% .[1:npag,1:25]
 clube[[9]]=page %>% html_node(".table") %>% html_nodes("img") %>% html_attr("alt")
-
-cb[[9]]
 
 clube[[9]]
 clube[[9]] %>% length() ->npag 
+
+cb[[9]]=page %>% html_node(".table") %>% html_table(fill=T) %>% .[1:npag,1:25]
+
+cb[[9]]
+
 
 jrodada=bind_rows(cb)
 jclube=flatten_chr(clube)
@@ -61,7 +67,7 @@ cb1[is.na(cb1)]=0
 cb2= cb1 %>% separate(Nome, into =  c("Nome","Posição"),sep=" \\(") %>% mutate(Posição=str_replace(Posição,"\\)",""))
 
 #Escrevendo os bancos de dados em CSV
-write.csv(cb,"CSV/camp_bras_rodada_20_bruto.csv",fileEncoding = 'ISO-8859-1')
-write.csv(cb1,"CSV/camp_bras_rodada_20_semi_bruto.csv",fileEncoding = 'ISO-8859-1')
-write.csv(cb2,"CSV/camp_bras_rodada_20_final.csv",fileEncoding = 'ISO-8859-1')
+write.csv(cb,paste0("CSV/camp_bras_rodada_",nrod,"_bruto.csv"),fileEncoding = 'ISO-8859-1')
+write.csv(cb1,paste0("CSV/camp_bras_rodada_",nrod,"_semi_bruto.csv"),fileEncoding = 'ISO-8859-1')
+write.csv(cb2,paste0("CSV/camp_bras_rodada_",nrod,"_final.csv"),fileEncoding = 'ISO-8859-1')
 
